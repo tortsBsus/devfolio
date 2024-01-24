d0cbc0c4f98063cf9860f28910f31879117fd587
tortsBsus <meganraj251@gmail.com>
2024-01-25 02:36:02 +0530

    Update assets with new color scheme

--- app/components/About/Logo.jsx
+++ app/components/About/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 function Logo() {
   return (
-    <div className="card h-full w-full bg-secondary shadow-xl">
+    <div className="card h-full w-full bg-primary shadow-xl">
   <div className="card-body">
     <div className='p-10'>
     <Image src="/assets/Logo.png" alt="me" fill="true" className="p-5" objectFit="contain" />
--- app/components/About/Skill.jsx
+++ app/components/About/Skill.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Skill(props) {
   return (
-    <div className="card h-full w-full bg-secondary shadow-xl">
+    <div className="card h-full w-full bg-primary shadow-xl">
     <div className="card-body flex flex-col justify-end text-heads5 lg:text-heads4 text-start text-neutral font-vergilia">
       {props.title}
     </div>
