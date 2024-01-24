ad583fdf5b5fefb7a01c0e76e645d668de195ea0
tortsBsus <meganraj251@gmail.com>
2024-01-25 02:34:51 +0530

    Update letter spacing and color

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <div className="hero min-h-screen w-full bg-base-100">
       <div className="hero-content flex justify-center align-middle text-center">
-        <h1 className="text-heads3 lg:text-titles text-center font-bold font-vergilia ">
+        <h1 className="text-heads3 lg:text-titles text-center font-bold font-vergilia tracking-wider text-neutral ">
           Portfolio
         </h1>
       </div>
