2884f34d4be25126f46cd42e2359e5196613d223
tortsBsus <meganraj251@gmail.com>
2024-03-11 22:07:42 +0530

    Update title

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,12 +2,17 @@ import '../styles/globals.css';
 import { Inter } from 'next/font/google';
 import { Source_Serif_4 } from 'next/font/google';
 
+
 const sourceSerif4 = Source_Serif_4({ subsets: ['latin'] });
 const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="peachy">
+      <head>
+        <title>Meghana's Devfolio </title>
+        
+      </head>
       <body className={sourceSerif4.className}>
         {children}
       
